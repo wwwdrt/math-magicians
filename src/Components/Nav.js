b7b50f95aae01371943570f918934b94f53eca3d
wwwdrt <juandapps@hotmail.com>
2023-06-15 21:30:19 -0500

    Add navbar component

---
+++ src/Components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav = () => (
+  <nav id="navbar">
+    <h1>Math Magicians</h1>
+    <ul id="menu">
+      <li><Link to="/">Home</Link></li>
+      <li>|</li>
+      <li><Link to="/calculator">Calculator</Link></li>
+      <li>|</li>
+      <li><Link to="/quotes">Quotes</Link></li>
+    </ul>
+  </nav>
+);
+
+export default Nav;
