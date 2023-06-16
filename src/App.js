eb19096711723f997654faab2c9afaaacfe6d172
wwwdrt <juandapps@hotmail.com>
2023-06-15 21:29:11 -0500

    Update main entry point

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Nav from './Components/Nav';
+import Home from './Components/Home';
 import Calculator from './Components/Calculator';
 import Quotes from './Components/Quotes';
 
-function App() {
-  return (
-    <>
-      <Quotes />
-      <Calculator />
-    </>
-  );
-}
+const App = () => (
+  <Router>
+    <Nav />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quotes" element={<Quotes />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
