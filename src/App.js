b1b538b91b6a90e4891fd2256f432a9881fd150e
wwwdrt <juandapps@hotmail.com>
2023-06-19 09:55:26 -0500

    Add import store & provider in app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import Nav from './Components/Nav';
 import Home from './Components/Home';
 import Calculator from './Components/Calculator';
 import Quotes from './Components/Quotes';
+import Login from './Components/Login';
 
 const App = () => (
-  <Router>
-    <Nav />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/calculator" element={<Calculator />} />
-      <Route path="/quotes" element={<Quotes />} />
-    </Routes>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quotes" element={<Quotes />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  </Provider>
 );
 
 export default App;
